07a5ede8e410c22e1c8ddc5154c1498f24885b01
Rolfknie <rolfknie12345@gmail.com>
2026-07-16 00:57:26 +0200

    Add files via upload

---
+++ screener.py
@@ -0,0 +1,16 @@
+[
+  {"ticker": "ABBV", "name": "AbbVie", "region": "US"},
+  {"ticker": "MTB", "name": "M&T Bank", "region": "US"},
+  {"ticker": "BPOP", "name": "Popular Inc", "region": "US"},
+  {"ticker": "VCTR", "name": "Victory Capital", "region": "US"},
+  {"ticker": "TRI", "name": "Thomson Reuters", "region": "US"},
+  {"ticker": "JNJ", "name": "Johnson & Johnson", "region": "US"},
+  {"ticker": "MS", "name": "Morgan Stanley", "region": "US"},
+  {"ticker": "EPD", "name": "Enterprise Products Partners", "region": "US"},
+  {"ticker": "WKL.AS", "name": "Wolters Kluwer", "region": "EU"},
+  {"ticker": "ASRNL.AS", "name": "ASR Nederland", "region": "EU"},
+  {"ticker": "MUV2.DE", "name": "Munich Re", "region": "EU"},
+  {"ticker": "6055.T", "name": "Japan Material", "region": "Asia"},
+  {"ticker": "7245.T", "name": "Daido Metal", "region": "Asia"},
+  {"ticker": "CNPF.PS", "name": "Century Pacific Food", "region": "Asia"}
+]
